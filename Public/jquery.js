5adc2b627aff1d0ea95c9d987541b8218bfe6971
Guillaume Verfaille <guillaume.verfaille@gmail.com>
2017-05-08 15:35:59 +0200

    add front end file into the Public folder

---
+++ Public/jquery.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+  $("#tableSecion").hide();
+  $("#inscriptionRow").hide();
+  $("#UEChoiceRow").hide();
+
+  $("#submit").click(function(){
+    var userid = $("#userid").val();
+    $("#tableSecion").show();
+    $("#inscriptionRow").show();
+    $("#titre").text("Bienvenue, "+userid+" voici vos informations");
+    //alert("submit clicked by "+userid);
+  });
+  $("#inscriptionButton").click(function(){
+    $("#UEChoiceRow").show();
+  });
+  $("#CancelUEChoiceButton").click(function(){
+    $("#UEChoiceRow").hide();
+  });
+});
+/*function check(form)/*function to check userid & password
+{
+
+alert("envoi de la requete nom:"+document.login.userid.value+" mdp:"+document.login.pswrd.value+"")/*displays error message
+
+}*/
