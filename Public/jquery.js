913db0c8b50287b057c803437843699a9ab086e2
Guillaume Verfaille <guillaume.verfaille@gmail.com>
2017-06-02 10:11:34 +0200

    modification, front end not finished because of corss platform error

--- Public/jquery.js
+++ Public/jquery.js
@@ -2,7 +2,10 @@ $(document).ready(function(){
     var students;
     var UEs;
     var ValidatedUE;
+    var ValidateUEID;
     var CurrentUE;
+    var CurrentUEID;
+    var firstClick=0;
     //-------------------------------------------Requête pour récupérer les students--------------------
     console.log("requête GET");
     $.ajax({
@@ -42,6 +45,7 @@ $(document).ready(function(){
     $("#tableSecion").hide();
     $("#inscriptionRow").hide();
     $("#UEChoiceRow").hide();
+    $("#inscriptionEntry").hide();
 
     $("#submit").click(function(){
         var userid = $("#userid");
@@ -52,18 +56,25 @@ $(document).ready(function(){
         $("#tableSecion").show();
         $("#inscriptionRow").show();
 
+        var offlineUEs = [{"id":18,"code":"2I5020","name":"Systèmes distribués","activities":[{"id":13,"name":"Sécurité physique et logicielle","code":"I502A","type":"course","hours":45,"local":""},{"id":14,"name":"Architecture distribuée","code":"I502L","type":"laboratory","hours":15,"local":""}],"prerequisites":[]},{"id":20,"code":"2I5040","name":"Infrastructure réseaux","activities":[{"id":15,"name":"Administration réseaux","code":"I504A","type":"course","hours":30,"local":""},{"id":16,"name":"Laboratoire d'administration réseaux","code":"I504B","type":"laboratory","hours":30,"local":""},{"id":17,"name":"Compléments de réseaux","code":"I504C","type":"course","hours":15,"local":""},{"id":18,"name":"Data center","code":"I504D","type":"course","hours":15,"local":""}],"prerequisites":[]},{"id":21,"code":"2I4080","name":"Applications mobiles","activities":[{"id":19,"name":"Applications mobiles","code":"2I4080","type":"laboratory","hours":45,"local":""}],"prerequisites":[20]},{"id":22,"code":"2I5010","name":"Applications web avancées","activities":[{"id":20,"name":"Applications web avancées","code":"2I5010","type":"laboratory","hours":30,"local":""}],"prerequisites":[]}];
+        var offlineStudents = [{"id":1,"matricule":"42","first_name":"Christophe","last_name":"Das Wulf","current_ues":"[]","validated_ues":"[]"},{"id":2,"matricule":"43","first_name":"Guillaume","last_name":"Verfaille","current_ues":"[332,334]","validated_ues":"[221,222]"}];
+        students = offlineStudents;
+        UEs = offlineUEs;
         //---------------------------------------------------------------------------------------------
         jQuery.each( students, function( key, value ) {
 
             if (value.matricule === userid.val()){
-                ValidatedUE = value.validated_ues;
-                CurrentUE = value.current_ues;
+                ValidatedUEID = value.validated_ues;
+                CurrentUEID = value.current_ues;
             }
             console.log(value.matricule);
         });
-        jQuery.each( allUE, function( key, value ) {
-            if (value.id === ValidatedEU[key].val()){
+        jQuery.each( UEs, function( key, value ) {
+            if (value.id === ValidatedUEID.val()){
                 ValidatedEU += value.validated_ues;
+
+            }
+            if (value.id === CurrentEUID.val()){
                 CurrentUE += value.current_ues;
             }
             console.log(value.ValidateUE);
@@ -78,23 +89,31 @@ $(document).ready(function(){
 
 
 $("#inscription").click(function(){
+    $("#inscriptionEntry").show();
     var userid = $("#userid");
     var fname = $("#fname");
     var lname = $("#lname");
-    //var step = "nok";
+    var step = "ok";
+
     console.log("matricule "+userid.val());
-    if(userid.val() !== "" && lname.val() !== "" && fname.val() !== ""){
-      console.log("requête GET");
+    if(userid.val() != "" && lname.val() != "" && fname.val() != ""){
+      console.log("no problemo");
       //--------------------------------------------------------------------------------------------------------
         jQuery.each( students, function( key, value ) {
             console.log( "key", key, "value", value );
             console.log(value.matricule);
+            if ((value.matricule != userid.val()) && (value.first_name != fname.val())){
+                step = "ok";
+            } else{
+                step = "nok";
+            }
         });
         //var student = Response;
         //console.log(student);
         //console.log(student.matricule);
         //step ="ok";
-        if (jQuery.isEmptyObject(Response)){
+        //if (jQuery.isEmptyObject(Response)){
+        if (step == "ok"){
             console.log("Requête POST");
             var formData = {first_name: fname.val(),last_name: lname.val(),matricule: userid.val()};
             $.ajax({
@@ -107,9 +126,18 @@ $("#inscription").click(function(){
                     console.log('La requête n\'a pas abouti'); }
             });
         }
+        $("#fname").val('');
+        $("#lname").val('');
         console.log("success");
-        console.log(Response);
+        //console.log(Response);
       //---------------------------------------------------------------------------------------------------------
+    }else{
+        if(firstClick == 1){
+            alert("veuillez compléter correctement toutes les informations");
+        } else{
+            firstClick = 1;
+        }
+
     }
 
 });
